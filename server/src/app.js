8cae06b4535fe09562233dc6b8f42984ffe4e7ff
Taras <protsyshyn.taras@coaxsoft.com>
2021-09-29 11:17:20 +0300

    feat: update common dev workflow

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,18 @@
+const path = require("path");
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
-app.use(express.json())
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("/planets", planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
