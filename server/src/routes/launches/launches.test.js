7bfc3b03e4a44c928df339ccef7e95e3982045d6
Taras <protsyshyn.taras@coaxsoft.com>
2021-10-03 20:38:38 +0300

    feat(server): connect mongo

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,55 +1,67 @@
 const request = require("supertest");
+
 const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("Test GET/launches", () => {
-  test("It should respond with 200 success", async () => {
-    await request(app).get("/launches").expect(200);
-  });
-});
-
-describe("Test POST/launches", () => {
-  const launchDataWithoutDate = {
-    mission: "Test",
-    rocket: "Explorer IS2",
-    target: "Kepler-168 f",
-  };
-
-  const launchDataWithInvalidDate = {
-    ...launchDataWithoutDate,
-    launchDate: "Hello",
-  };
-
-  const launchData = {
-    ...launchDataWithoutDate,
-    launchDate: "January 16, 2030",
-  };
-
-  test("It should respond with 201 success", async () => {
-    const res = await request(app)
-      .post("/launches")
-      .send(launchData)
-      .expect("Content-Type", /json/)
-      .expect(201);
-    const reqDate = new Date(launchData.launchDate).valueOf();
-    const resDate = new Date(res.body.launchDate).valueOf();
-
-    expect(reqDate).toBe(resDate);
-    expect(res.body).toMatchObject(launchDataWithoutDate);
+describe("Test Launces API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
 
-  test("It should catch missing require properties", async () => {
-    await request(app)
-      .post("/launches")
-      .send(launchDataWithoutDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
+  afterAll(async () => {
+    await mongoDisconnect();
   });
 
-  test("It should catch invalid date", async () => {
-    await request(app)
-      .post("/launches")
-      .send(launchDataWithInvalidDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
+  describe("Test GET/launches", () => {
+    test("It should respond with 200 success", async () => {
+      await request(app).get("/launches").expect(200);
+    });
+  });
+
+  describe("Test POST/launches", () => {
+    const launchDataWithoutDate = {
+      mission: "Test",
+      rocket: "Explorer IS2",
+      target: "Kepler-1652 b",
+    };
+
+    const launchDataWithInvalidDate = {
+      ...launchDataWithoutDate,
+      launchDate: "Hello",
+    };
+
+    const launchData = {
+      ...launchDataWithoutDate,
+      launchDate: "January 16, 2030",
+    };
+
+    test("It should respond with 201 success", async () => {
+      const res = await request(app)
+        .post("/v1/launches")
+        .send(launchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+      const reqDate = new Date(launchData.launchDate).valueOf();
+      const resDate = new Date(res.body.launchDate).valueOf();
+
+      expect(reqDate).toBe(resDate);
+      expect(res.body).toMatchObject(launchDataWithoutDate);
+    });
+
+    test("It should catch missing require properties", async () => {
+      await request(app)
+        .post("/v1/launches")
+        .send(launchDataWithoutDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+    });
+
+    test("It should catch invalid date", async () => {
+      await request(app)
+        .post("/v1/launches")
+        .send(launchDataWithInvalidDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+    });
   });
 });
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,20 @@
 const http = require("http");
 
+require("dotenv").config();
+
 const app = require("./app");
 const { loadPlanetData } = require("./models/planets.models");
+const { loadLaunchesData } = require("./models/launches.model");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoConnect();
   await loadPlanetData();
+  await loadLaunchesData();
 
   server.listen(PORT, () => {
     console.log(`Server was started on: ${PORT}`);
