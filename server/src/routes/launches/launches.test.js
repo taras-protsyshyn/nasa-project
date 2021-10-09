796a8bd216d7e96fa9cc9cda315c06bed3a3bfab
Taras <protsyshyn.taras@coaxsoft.com>
2021-10-09 18:31:27 +0300

    fix(server): lodad planets data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,10 +2,12 @@ const request = require("supertest");
 
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetData } = require("../../models/launches.mongo");
 
 describe("Test Launces API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetData();
   });
 
   afterAll(async () => {
